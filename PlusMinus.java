ebae961974128e3bc142a2f2be0f9ddfea0eb098
tharun-abishek <105432952+tharun-abishek@users.noreply.github.com>
2022-05-20 14:22:03 +0530

    Add files via upload

---
+++ PlusMinus.java
@@ -0,0 +1,26 @@
+public class PlusMinus{
+   public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            int positive = 0;
+            int zero = 0;
+            int negative = 0;
+             for (int i=0 ; i<n ; i++){
+                int x = in.nextInt();
+                if (x>0){
+                    positive++;
+                }
+                else if (x==0){
+                    zero++;
+                }
+                else{
+                    negative++;
+                }       
+            }
+            System.out.println(positive /(double) n);
+            System.out.println(negative /(double) n);
+            System.out.println(zero /(double) n);
+        
+        
+    }
+}
